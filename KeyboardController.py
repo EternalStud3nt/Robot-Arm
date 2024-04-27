ced42dc7c649a434b2f1fe556a8579bd5b1066d8
Eternal Student <eternalstud3nt@gmail.com>
2024-04-27 20:13:22 +0300

    Updated keyboard contro

--- KeyboardController.py
+++ KeyboardController.py
@@ -1,4 +1,4 @@
-import keyboard
+from pynput import keyboard
 
 class KeyboardController:
     def __init__(self):
@@ -12,18 +12,20 @@ class KeyboardController:
             'f': False,
             'j': False
         }
-        for key in self.keys:
-            keyboard.on_press_key(key, self._set_key_down, suppress=False)
-            keyboard.on_release_key(key, self._set_key_up, suppress=False)
+        self.listener = keyboard.Listener(
+            on_press=self._set_key_down,
+            on_release=self._set_key_up)
+        self.listener.start()
 
-    def _set_key_down(self, e):
-        if e.name in self.keys:
-            self.keys[e.name] = True
+    def _set_key_down(self, key):
+        key_name = str(key).replace("'", "")
+        if key_name in self.keys:
+            self.keys[key_name] = True
 
-    def _set_key_up(self, e):
-        if e.name in self.keys:
-            self.keys[e.name] = False
+    def _set_key_up(self, key):
+        key_name = str(key).replace("'", "")
+        if key_name in self.keys:
+            self.keys[key_name] = False
 
     def is_key_pressed(self, key):
         return self.keys.get(key, False)
-
