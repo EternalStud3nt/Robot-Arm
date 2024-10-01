cd986daea771145a69e92975c5bcc07db5363830
EternalStud3nt <eternalStud3nt@gmail.com>
2024-10-01 18:23:37 +0300

    updated controller

--- controller.py
+++ controller.py
@@ -2,18 +2,60 @@ from motor import Motor
 import time
 from pynput import keyboard
 
+rotation_speed = 30  # degrees per second
+
+def on_press(key):
+    global rotating_left, rotating_right, last_time
+    if key == keyboard.Key.left:
+        rotating_left = True
+        last_time = time.time()  # Record the time when key is pressed
+    elif key == keyboard.Key.right:
+        rotating_right = True
+        last_time = time.time()
+
+def on_release(key):
+    global rotating_left, rotating_right
+    if key == keyboard.Key.left:
+        rotating_left = False
+    elif key == keyboard.Key.right:
+        rotating_right = False
+
+# Debug function for manual angle input
+def manual_control():
+    while True:
+        pulse = input("Enter angle: ")
+        pulse = int(pulse)
+        motor_0.set_rotation(pulse)
+
 if __name__ == "__main__":
     
     print("Welcome to the controller...")
     motor_0 = Motor(0)
-    motor_1 = Motor(1)
-    motor_2 = Motor(2)
-    motor_3 = Motor(3)
-    motors = [motor_0, motor_1, motor_2, motor_3]
     motor_0.set_rotation(90)
     
-    while(True):
-        pulse = input("enter angle: ")
-        pulse = int(pulse)
-        motor_0.set_rotation(pulse)
-    
+    rotating_left = False
+    rotating_right = False
+    last_time = time.time()
+
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+
+    try:
+        while True:
+            current_time = time.time()
+            time_delta = current_time - last_time  # Time since last update
+
+            if rotating_left:
+                motor_0.rotate(-rotation_speed * time_delta)  # Rotate left
+            elif rotating_right:
+                motor_0.rotate(rotation_speed * time_delta)  # Rotate right
+            
+            last_time = current_time  # Update the last_time
+            time.sleep(0.01)  # Small delay to avoid high CPU usage
+
+        # Uncomment the following line to use the debug function:
+        # manual_control()
+
+    except KeyboardInterrupt:
+        listener.stop()
+        print("\nController stopped.")
