d05db1d1947e911e9955086fd709e542bf18a3ac
EternalStud3nt <eternalStud3nt@gmail.com>
2024-10-01 17:15:09 +0300

    bug fix

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 from motor import Motor
 import time
-import keyboard
+from pynput import keyboard
 
 if __name__ == "__main__":
     
@@ -10,22 +10,28 @@ if __name__ == "__main__":
     motor_2 = Motor(2)
     motor_3 = Motor(3)
     motors = [motor_0, motor_1, motor_2, motor_3]
-    
-    for motor in motors:
-        time.sleep(1)
-        motor.set_rotation(90)
+    motor_0.set_rotation(0)
 
-    def rotate_motor():
-        motor = Motor(0)
-        while True:
-            
-            if keyboard.is_pressed('left'):
+    motor = Motor(0)
+
+    def on_press(key):
+        try:
+            if key == keyboard.Key.left:  # Left arrow key
                 motor.rotate(-1)
                 print("Rotating left")
-                time.sleep(0.04)
-            elif keyboard.is_pressed('right'):
-                motor.set_rotation(+1)
+                time.sleep(0.04)  # Pause to avoid rapid rotation
+            elif key == keyboard.Key.right:  # Right arrow key
+                motor.rotate(+1)
                 print("Rotating right")
-                time.sleep(0.04)
+                time.sleep(0.04)  # Pause to avoid rapid rotation
+        except AttributeError:
+            pass
 
-    rotate_motor()
+    def on_release(key):
+        if key == keyboard.Key.esc:
+            # Stop listener on 'esc' press
+            return False
+
+    # Start listening for keypresses
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
