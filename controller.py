791abf5d5ad1c32590922e1f23cb1239fc5b7c51
EternalStud3nt <eternalStud3nt@gmail.com>
2024-10-01 16:40:35 +0300

    resolved merge conflicts

--- controller.py
+++ controller.py
@@ -1,15 +1,31 @@
 from motor import Motor
 import time
+import keyboard
 
-if (__name__ == "__main__"):
-	print("Welcome to the controller...")
-	
-	motor_0 = Motor(0)
-	motor_1 = Motor(1)
-	motor_2 = Motor(2)
-	motor_3 = Motor(3)
-	motors = [motor_0, motor_1, motor_2, motor_3]
-	
-	for motor in motors:
-		time.sleep(1)
-		motor.set_rotation(90)
+if __name__ == "__main__":
+    
+    print("Welcome to the controller...")
+    motor_0 = Motor(0)
+    motor_1 = Motor(1)
+    motor_2 = Motor(2)
+    motor_3 = Motor(3)
+    motors = [motor_0, motor_1, motor_2, motor_3]
+    
+    for motor in motors:
+        time.sleep(1)
+        motor.set_rotation(90)
+
+    def rotate_motor():
+        motor = Motor(0)
+        while True:
+            
+            if keyboard.is_pressed('left'):
+                motor.rotate(-1)
+                print("Rotating left")
+                time.sleep(0.04)
+            elif keyboard.is_pressed('right'):
+                motor.set_rotation(+1)
+                print("Rotating right")
+                time.sleep(0.04)
+
+    rotate_motor()
--- motor.py
+++ motor.py
@@ -3,22 +3,16 @@ import conversions
 
 class Motor:
     def __init__(self, channel):
-        self.rotation = 0
-        self.pwm = PCA9685()
         self.channel = channel
-        self.set_rotation(0)
-    
-    def set_rotation(self, angle):
-        pulse = conversions.angle_to_pulse(angle)
-        self.pwm.setServoPulse(self.channel, pulse)
-        print("Sending a pulse of: " + str(pulse))
-        self.set_rotation(0)
+        self.pwm = PCA9685()
+        self.rotation = 0
+        self.set_rotation(90)
     
     def set_rotation(self, angle):
         pulse = conversions.angle_to_pulse(angle)
         self.pwm.setServoPulse(self.channel, pulse)
         self.rotation = angle
-        print("Sending a pulse of: " + pulse)
+        print("Sending a pulse of: " + str(pulse))  # Convert pulse to string
     
     def rotate(self, delta_angle):
         new_angle = self.rotation + delta_angle
