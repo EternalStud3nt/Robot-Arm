689c3c566455ec4cb49e15705a8803960d85dc63
EternalStud3nt <stavroskaskuras@gmail.com>
2024-12-05 18:18:25 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 if __name__ == "__main__":
-    from tic_tac_toe.game_manager import GameManager
-    gameManager = GameManager()
-    gameManager.start_game()
-    
+    from robot_arm.controller import Controller  
+    controller = Controller()
+#     from tic_tac_toe.game_manager import GameManager
+#     gameManager = GameManager()
+#     gameManager.start_game()
