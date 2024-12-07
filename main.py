29aa1d3a93508fd2cd8911c352e7bdf710ed8b7e
EternalStud3nt <eternalstud3nt@gmail.com>
2024-12-07 13:33:42 +0200

    refactor: restructure robot control logic into separate function

--- main.py
+++ main.py
@@ -4,32 +4,26 @@ from tic_tac_toe.game_manager import GameManager
 from robot_arm.arm_controller import ArmController
 from robot_arm.robot_arm import RobotArm
 
-if __name__ == "__main__":
-    #controller = ArmController()
-#     arm = RobotArm()
-#     while True:
-#         try:
-#             depth = float(input("Enter depth: "))
-#             height = float(input("Enter height: "))
-#             rotation = float(input("Enter base rotation: "))
-#         except ValueError:
-#             print("Invalid input. Please enter numeric values.")
-#         else:
-#             arm.set_depth(depth)
-#             arm.set_height(height)
-#             arm.set_rotation_xz(rotation)
+def control_robot():
+    arm = RobotArm()
+    while True:
+        try:
+            depth = float(input("Enter depth: "))
+            height = float(input("Enter height: "))
+            rotation = float(input("Enter base rotation: "))
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
+        else:
+            arm.set_depth(depth)
+            arm.set_height(height)
+            arm.set_rotation_xz(rotation)
+
+def test_robot_positioning():
     grid = Grid()
     game_manager = GameManager()
     ai = AI(1, game_manager, grid)
     ai.test_set_positions()
-#     
 
-    
-#     # controller = ArmController()
-    
-    
-#     # ai.grab_piece()
-#     # from robot_arm.arm_controller import ArmController  
-# #     from tic_tac_toe.game_manager import GameManager
-# #     gameManager = GameManager()
-# #     gameManager.start_game()
+if __name__ == "__main__":
+    control_robot()
+    #test_robot_positioning()
