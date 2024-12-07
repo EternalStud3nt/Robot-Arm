c558d4992222f2ea070df2ce46c87b7e1687cda9
EternalStud3nt <eternalstud3nt@gmail.com>
2024-12-07 21:00:20 +0200

    fix: optimize servo rotation for smoother transitions

--- robot_arm/motor.py
+++ robot_arm/motor.py
@@ -13,20 +13,18 @@ class Motor:
         if(target_rotation >= 180): target_rotation = 180
         if(target_rotation <= 0): target_rotation = 0
         
-        total_steps = abs(target_rotation - self.rotation)
-        total_time = 2  # Total time in seconds for the entire rotation
-        step_time = total_time / total_steps if total_steps != 0 else 0
+        diff = target_rotation - self.rotation
         
-        step = 1 if target_rotation > self.rotation else -1
-        for angle in range(self.rotation, target_rotation, step):
-            target_pulse = conversions.angle_to_pulse(angle)
-            self.pwm.setServoPulse(self.channel, target_pulse)
-            self.rotation = angle
-            time.sleep(step_time)  # Adjust the sleep time for desired speed
+        new_rotation = self.rotation + diff/2
+        pulse = conversions.angle_to_pulse(new_rotation)
+        self.pwm.setServoPulse(self.channel, pulse)
+        self.rotation = new_rotation
+        time.sleep(0.2)
         
-        
-        self.pwm.setServoPulse(self.channel, target_pulse)
-        self.rotation = target_rotation
+        new_rotation = target_rotation
+        pulse = conversions.angle_to_pulse(new_rotation)
+        self.pwm.setServoPulse(self.channel, pulse)
+        self.rotation = new_rotation
     
     def rotate(self, delta_angle):
         new_angle = self.rotation + delta_angle
