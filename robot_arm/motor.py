701a98c5315b68afd9edf9c3719f1621851e8fe2
EternalStud3nt <eternalstud3nt@gmail.com>
2024-12-07 20:39:04 +0200

    fix: enhance set_rotation method for smoother servo movement

--- robot_arm/motor.py
+++ robot_arm/motor.py
@@ -1,6 +1,6 @@
 from robot_arm.PCA9685 import PCA9685
-import time
 import conversions
+import time
 
 class Motor:
     def __init__(self, channel, pca):
@@ -9,21 +9,26 @@ class Motor:
         self.rotation = 0
         self.set_rotation(90)
     
-    def set_rotation(self, angle):
-        if(angle >= 180): angle = 180
-        if(angle <= 0): angle = 0
+    def set_rotation(self, target_rotation):
+        if(target_rotation >= 180): target_rotation = 180
+        if(target_rotation <= 0): target_rotation = 0
         
-        pulse = conversions.angle_to_pulse(angle)
-        
-        # Send pulse over 50 steps to simulate smooth movement
         current_pulse = conversions.angle_to_pulse(self.rotation)
-        step = (pulse - current_pulse) / 50  # 50 steps for 1 second
-        for i in range(50):
-            current_pulse += step
+        target_pulse = conversions.angle_to_pulse(target_rotation)
+        
+        print(f"Current pulse: {current_pulse}, target pulse: {target_pulse}, target rotation: {target_rotation}")
+        
+        step_count = 50
+        step_delay = 1.0 / step_count
+        step_size = (target_pulse - current_pulse) / step_count
+        
+        for step in range(step_count):
+            current_pulse += step_size
             self.pwm.setServoPulse(self.channel, current_pulse)
-            time.sleep(0.02)  # 20 ms per step
-            
-        self.rotation = angle
+            time.sleep(step_delay)
+        
+        self.pwm.setServoPulse(self.channel, target_pulse)
+        self.rotation = target_rotation
     
     def rotate(self, delta_angle):
         new_angle = self.rotation + delta_angle
