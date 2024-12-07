2133a77588e5519f25cd5b74d127b5faa8be1133
EternalStud3nt <eternalstud3nt@gmail.com>
2024-12-07 21:28:55 +0200

    fix: implement gradual servo rotation for smoother transitions

--- robot_arm/motor.py
+++ robot_arm/motor.py
@@ -8,23 +8,26 @@ class Motor:
         self.pwm = pca
         self.rotation = 0
         self.set_rotation(90)
+        self.angular_speed = 67.5  # degrees per second
     
     def set_rotation(self, target_rotation):
         if(target_rotation >= 180): target_rotation = 180
         if(target_rotation <= 0): target_rotation = 0
         
-        diff = target_rotation - self.rotation
+        delta_degree = self.angular_speed * 0.02
+        angular_diff = target_rotation - self.rotation
+        steps = int(abs(angular_diff) / delta_degree)
         
-        new_rotation = self.rotation + diff/2
-        pulse = conversions.angle_to_pulse(new_rotation)
+        for i in range(steps):
+            new_rotation = self.rotation + delta_degree
+            pulse = conversions.angle_to_pulse(new_rotation)
+            self.pwm.setServoPulse(self.channel, pulse)
+            self.rotation = new_rotation
+            time.sleep(0.02)
+            
+        pulse = conversions.angle_to_pulse(target_rotation)
         self.pwm.setServoPulse(self.channel, pulse)
-        self.rotation = new_rotation
-        time.sleep(0.2)
-        
-        new_rotation = target_rotation
-        pulse = conversions.angle_to_pulse(new_rotation)
-        self.pwm.setServoPulse(self.channel, pulse)
-        self.rotation = new_rotation
+        self.rotation = target_rotation
     
     def rotate(self, delta_angle):
         new_angle = self.rotation + delta_angle
