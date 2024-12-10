b756d3f4300dc412797c6756554a76472e53b495
EternalStud3nt <eternalstud3nt@gmail.com>
2024-12-10 18:17:06 +0200

    feat: enhance object detection by adding filtering option and implement grid area detection

--- computer_vision/image_processor.py
+++ computer_vision/image_processor.py
@@ -5,7 +5,12 @@ class ImageProcessor:
     def __init__(self):
         self.model = YOLO("last.pt")
 
-    def detect_objects(self, frame):
+    def detect_objects(self, frame, name=None):
+        """
+        Detects objects in the frame and returns the labels and bounding boxes. 
+        If a name is provided, only objects with that name are returned.
+        """
+        
         results = self.model(frame)
         detections = []
         for result in results:
@@ -15,16 +20,37 @@ class ImageProcessor:
                 confidence = box.conf[0]
                 if confidence > 0.3:
                     detections.append((label, (x1, y1, x2, y2)))
+        
+        if name:
+            detections = [box for box in detections if box[0] == name]
+        
         return detections
 
-    def detect_object(self, frame, name):
-        detections = self.detect_objects(frame)
+    def detect_grid_area(self, frame):
+        """
+        Detects the grid area in the frame and return the top left and bottom right coordinates
+        """
         
-        target_objects = []
-        for box in detections:
-            if box[0] == name:
-                target_objects.append(box)
-        return target_objects
+        detected_cells = self.detect_objects(frame, "Cell")
+        
+        # Detect the top left and bottom right cell
+        top_left_cell = None
+        bottom_right_cell = None
+        
+        for cell in detected_cells:
+            x1, y1, x2, y2 = cell[1]
+            if top_left_cell is None:
+                top_left_cell = (x1, y1)
+            else:
+                top_left_cell = (min(top_left_cell[0], x1), min(top_left_cell[1], y1))
+                
+            if bottom_right_cell is None:
+                bottom_right_cell = (x2, y2)
+            else:
+                bottom_right_cell = (max(bottom_right_cell[0], x2), max(bottom_right_cell[1], y2))
+                
+        # return the top left and bottom right cell coordinates
+        return top_left_cell, bottom_right_cell
 
     def draw_objects(self, frame):
         detections = self.detect_objects(frame)
@@ -38,5 +64,3 @@ class ImageProcessor:
             cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
             cv2.putText(frame, f"{label}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         return frame
-
-
