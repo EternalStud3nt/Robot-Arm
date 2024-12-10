3f5a155e3acd89a1e610d12dc499a2a8a85b3701
EternalStud3nt <eternalstud3nt@gmail.com>
2024-12-10 18:01:24 +0200

    feat: enhance object detection and drawing functionality in image processor

--- computer_vision/image_processor.py
+++ computer_vision/image_processor.py
@@ -5,21 +5,38 @@ class ImageProcessor:
     def __init__(self):
         self.model = YOLO("last.pt")
 
-    def draw_objects(self, frame):
+    def detect_objects(self, frame):
         results = self.model(frame)
+        detections = []
         for result in results:
             for box in result.boxes:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 label = result.names[int(box.cls)]
                 confidence = box.conf[0]
+                if confidence > 0.3:
+                    detections.append((label, (x1, y1, x2, y2)))
+        return detections
 
-                colors = {
-                    0: (255, 0, 0),
-                    1: (0, 255, 0),
-                    2: (0, 0, 255),
-                }
+    def detect_object(self, frame, name):
+        detections = self.detect_objects(frame)
+        
+        target_objects = []
+        for box in detections:
+            if box[0] == name:
+                target_objects.append(box)
+        return target_objects
 
-                color = colors.get(int(box.cls), (255, 255, 255))
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                cv2.putText(frame, f"{label} ({confidence * 100:.1f}%)", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, color, 1)
+    def draw_objects(self, frame):
+        detections = self.detect_objects(frame)
+        for label, (x1, y1, x2, y2) in detections:
+            colors = {
+                'X': (0, 0, 255),
+                'O': (0, 255, 0),
+                'Cell': (255, 255, 255),
+            }
+            color = colors.get(label, (255, 255, 255))
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame, f"{label}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         return frame
+
+
